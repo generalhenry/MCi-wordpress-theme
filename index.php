4a27be88eb4372ca02f32df4c58b1f64a3bc24a0
Henry Allen-Tilford <htilford@gmail.com>
2011-12-19 17:21:49 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,16 @@
 
 	<?php while (have_posts()) : the_post(); ?>
 
-		<div class="post" id="post-<?php the_ID(); ?>">
+		<div class="post-sidebar">
+			<div class="post-date">
+				<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+					<span class="post-month"><?php the_time('M'); ?></span>
+					<span class="post-day"><?php the_time('j'); ?></span>
+				</a>
+			</div>
+			<div class="edit-link">edit</div>
+		</div>
+		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<h2><?php the_title(); ?></h2>
 
 			<div class="entry">
