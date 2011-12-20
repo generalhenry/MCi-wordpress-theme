e916d009fdb920df97a8fc6f959c5b894a02f05e
Henry Allen-Tilford <htilford@gmail.com>
2011-12-19 17:06:51 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,10 @@
     <footer>
-
+        <?php wp_footer(); ?>
     </footer>
   </div> <!--! end of #container -->
 
 
   <!-- JavaScript at the bottom for fast page loading -->
-
-  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
-
-  <!-- scripts concatenated and minified via ant build script-->
-  <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
-  <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
-  <!-- end scripts-->
 
   <!-- mathiasbynens.be/notes/async-analytics-snippet Change UA-XXXXX-X to be your site's ID -->
   <script>
