1a55e5ddb6774774774eccc87f4d7dcab41ee198
Henry Allen-Tilford <htilford@gmail.com>
2011-12-19 17:00:09 -0800

    Update header.php

--- header.php
+++ header.php
@@ -36,5 +36,7 @@
 
   <div id="container">
     <header>
-
+      <h1 class="title"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+      <div id="search" <?php if ( $bnd_search == 1 || $bnd_quickblog == 1 ) { echo "style='display: none'"; } ?>><?php get_search_form(); ?></div>
+    	<h2 class="tagline"><?php bloginfo('description'); ?></h2>
     </header>
