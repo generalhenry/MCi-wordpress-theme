99fc3a8b5e41e2512e6956bc6eb36086100534ad
Henry Allen-Tilford <htilford@gmail.com>
2011-12-22 17:14:02 -0800

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 				<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
 					<span class="post-month"><?php the_time('M'); ?></span>
 					<span class="post-day"><?php the_time('j'); ?></span>
+					<div class="post-year"><?php the_year('M'); ?></div>
 				</a>
 			</div>
 		</div>
